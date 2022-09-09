05974a91cad0a1b0a55fe73f84300c3e9cd35c51
uabinesh <79247690+uabinesh@users.noreply.github.com>
2022-09-09 18:10:48 +0530

    routes updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import * as React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {ToastContainer} from "react-toastify";
 
 import ChannelingDoctor from "./pages/ChannelingDoctor";
 import BookAppointment from "./pages/BookAppointment";
 import AppointmentList from "./pages/AppointmentList";
+import FoodMenu from "./pages/FoodMenu";
+import AddFood from "./pages/AddFood";
 
 
 function App() {
@@ -15,7 +18,11 @@ function App() {
          <Route path="/123" element={<BookAppointment/>}/>
          <Route path="/321" element={<AppointmentList/>}/>
 
+         <Route path="/foodmenu" element={<FoodMenu/>}/>
+         <Route path="/addfood" element={<AddFood/>}/>
+
        </Routes>
+        <ToastContainer style={{ width: "400px" }}/>
     </BrowserRouter>
   );
 }
