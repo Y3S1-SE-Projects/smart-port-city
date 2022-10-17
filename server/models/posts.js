95694d426668ac9616ad6a7562801a819964152d
chandur-surge <chandur@surge.global>
2022-10-17 20:39:26 +0530

    Added schema model

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
+// Creating objects of data type/structure in the document to be stored in the MongoDB
+const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
-
-    topic:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    postCategory:{
-        type:String,
-        required:true       
-    },
-    date:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:String,
-        required:true
-    },
-    cardno:{
-        type:String,
-        required:true
-    },
-    cvv:{
-        type:String,
-        required:true
-    }
+  topic: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  postCategory: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  cardno: {
+    type: String,
+    required: true,
+  },
+  cvv: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Posts', postSchema);
+module.exports = mongoose.model("Posts", postSchema);
