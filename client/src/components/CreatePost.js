df671030bc001dec07fa5d182f344e8b1acbccdd
chandur-surge <chandur@surge.global>
2022-10-18 17:20:52 +0530

    Defining validation schema for properties

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,139 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
 import axios from "axios";
 import Footer from "../components/Footer";
 
-const initialState = {
-  namerr: "",
-  descriptionerr: "",
-  postCategoryerr: "",
-  dateerr: "",
-  priceerr: "",
-  cardnoerr: "",
-  cvverr: "",
-};
+// Validation using the Yup object out of the box methods
+const validationSchema = Yup.object({
+  topic: Yup.string()
+    .typeError("Please enter only letters in the feild")
+    .max(25, "Maximum character limit (25) reached !")
+    .required("topic is required"),
+  description: Yup.string()
+    .typeError("Please enter only letters in the feild")
+    .max(50, "Maximum character limit (50) reached !")
+    .required("Description is required"),
+  postCategory: Yup.string().required("PostCategory is required"),
+  date: Yup.date()
+    .min("10/22/2022", "Date annot be in the past")
+    .max("10/22/2050", "Unavailable, please select a year less than 2050 !")
+    .required("Date is required"),
+  price: Yup.number()
+    .typeError("Please enter only numbers in the feild")
+    .positive("Please enter only positive integer numbers")
+    .min(1000, "Minimum is Rs/- 1000")
+    .max(10000, "Maximum is Rs/- 10000")
+    .required("Price is required"),
+  cardno: Yup.number()
+    .typeError("Please enter only numbers in the feild")
+    .positive("Please enter only positive integer numbers")
+    .min(123456789012, "CVV must be 3 digits")
+    .max(210987654321, "CVV must be 3 digits")
+    .required("Card Number is required"),
+  cvv: Yup.number()
+    .typeError("Please enter only numbers in the feild")
+    .positive("Please enter only positive integer numbers")
+    .min(100, "CVV must be 3 digits")
+    .max(999, "CVV must be 3 digits")
+    .required("CVV is required"),
+});
 
-export default class CreatePost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      topic: "",
-      description: "",
-      postCategory: "",
-      date: "",
-      price: "",
-      cardno: "",
-      cvv: "",
-    };
-  }
-
-  state = initialState;
-
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-
-    this.setState({
-      ...this.state,
-      [name]: value,
-    });
-  };
-
-  // Validation function
-  validate = () => {
-    let namerr = "";
-    let descriptionerr = "";
-    let postCategoryerr = "";
-    let dateerr = "";
-    let priceerr = "";
-    let cardnoerr = "";
-    let cvverr = "";
-
-    // error states
-    if (!this.state.topic) {
-      namerr = "Name cannot be blank";
-    }
-    if (!this.state.description) {
-      descriptionerr = "Sport Event cannot be blank";
-    }
-    if (!this.state.postCategory) {
-      postCategoryerr = "Type of sport cannot be blank";
-    }
-    if (!this.state.date) {
-      dateerr = "Date cannot be blank";
-    }
-    if (!this.state.price) {
-      priceerr = "Price cannot be blank";
-    }
-    // if(this.state.price != "[0-9]{10}") {
-    //   priceerr = "Please enter only numbers";
-    // }
-    if (!this.state.cardno) {
-      cardnoerr = "Card Number cannot be blank";
-    }
-    // if(this.state.cardno != /^\d{12}$/) {
-    //   cardnoerr = "Please enter your 12 digit credit card number correctly";
-    // }
-    if (!this.state.cvv) {
-      cvverr = "CVV cannot be blank";
-    }
-    // if(this.state.cvv != /^\d{3}$/) {
-    //   cvverr = "Please enter your 3 digit cvv correctly";
-    // }
-
-    if (
-      namerr ||
-      descriptionerr ||
-      postCategoryerr ||
-      dateerr ||
-      priceerr ||
-      cardnoerr ||
-      cvverr
-    ) {
-      this.setState({
-        namerr,
-        descriptionerr,
-        postCategoryerr,
-        dateerr,
-        priceerr,
-        cardnoerr,
-        cvverr,
-      });
-      return false;
-    }
-
-    return true;
-  };
-
-  onSubmit = (e) => {
-    e.preventDefault();
-
-    const { topic, description, postCategory, date, price, cardno, cvv } =
-      this.state;
-    const isValid = this.validate();
-    if (isValid) {
-      console.log(this.state);
-
-      // clear form
-      this.setState(initialState);
-    }
-
-    const data = {
-      topic: topic,
-      description: description,
-      postCategory: postCategory,
-      date: date,
-      price: price,
-      cardno: cardno,
-      cvv: cvv,
-    };
-
-    console.log(data);
-
-    axios
-      .post("https://smart-port-city-fullstack.herokuapp.com/post/save", data)
-      .then((res) => {
-        if (res.data.success) {
-          alert("Record Saved Successfully");
-          this.setState({
+const CreatePost = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <>
+      <div class="d-flex justify-content-center p-5">
+        <Formik
+          initialValues={{
             topic: "",
             description: "",
             postCategory: "",
@@ -141,147 +53,40 @@ export default class CreatePost extends Component {
             price: "",
             cardno: "",
             cvv: "",
-          });
-        }
-      });
-  };
+          }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            console.log(values);
+            await axios
+              .post("http://localhost:8000/post/save", values)
+              .then((res) => {
+                localStorage.setItem("authToken", res.data.token);
+                localStorage.setItem("userRole", res.data.user.role);
+                setIsLoading(false);
+                if (res.data.user.role === "customer") {
+                  window.location = `/customer`;
+                }
+              })
+              .catch((err) => {
+                alert("Payment is successfully added!");
+                window.location = "/";
+              });
+          }}
+        >
+          {({ handleChange, handleSubmit, values, errors, touched }) => (
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                setIsLoading(true);
+                handleSubmit();
+              }}
+            ></form>
+          )}
+        </Formik>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <div className="tablebg">
-          <div className="col-md-8 mt-4 mx-auto">
-            <h1
-              className="h3 mb-3 font-weight-normal"
-              style={{ marginTop: "8%", paddingTop: "50px" }}
-            >
-              Purchase a new ticket
-            </h1>
-            <form className="needs-validation">
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Full Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="topic"
-                  placeholder="Enter Topic"
-                  value={this.state.topic}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.namerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Sporting Event</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="description"
-                  placeholder="Enter Topic"
-                  value={this.state.description}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.descriptionerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Type of Sport</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="postCategory"
-                  placeholder="Enter Sport"
-                  value={this.state.postCategory}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.postCategoryerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Date</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="date"
-                  placeholder="Enter Date"
-                  value={this.state.date}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.dateerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Price</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="price"
-                  placeholder="Enter Price"
-                  value={this.state.price}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.priceerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>Card No</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="cardno"
-                  placeholder="Enter Card Number"
-                  s
-                  value={this.state.cardno}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.cardnoerr}
-                </div>
-              </div>
-              <div className="form-group" style={{ marginBottom: "15px" }}>
-                <label style={{ marginBottom: "5px" }}>CVV</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="cvv"
-                  placeholder="Enter CVV"
-                  value={this.state.cvv}
-                  onChange={this.handleInputChange}
-                />
-                <div style={{ font: 12, color: "red" }}>
-                  {this.state.cvverr}
-                </div>
-              </div>
-              <button
-                className="btn btn-success"
-                type="submit"
-                style={{ marginTop: "15px" }}
-                onClick={this.onSubmit}
-              >
-                <i className="far fa-check-square"></i>
-                &nbsp; Pay Now
-              </button>
-              <button
-                className="btn btn-primary"
-                style={{ marginLeft: "10px", marginTop: "15px" }}
-              >
-                <a
-                  href="/table"
-                  style={{ textDecoration: "none", color: "white" }}
-                >
-                  Back
-                </a>
-              </button>{" "}
-            </form>
-          </div>
-          <Footer />
-        </div>{" "}
-      </>
-    );
-  }
-}
+export default CreatePost;
