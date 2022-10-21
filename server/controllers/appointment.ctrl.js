9f30edcdfc673d2ba68be4356c8285f19f90dab1
ZIHARA M.I.F <86686054+Zihara@users.noreply.github.com>
2022-10-21 21:38:34 +0530

    Appointment Controller updated

--- server/controllers/appointment.ctrl.js
+++ server/controllers/appointment.ctrl.js
@@ -4,8 +4,16 @@ const Appointment = require("../models/Appointment.model");
 const createAppointment = (req,res)=>{
 
     console.log(`<-- ${req.method} Request`);
-    const appointment = new Appointment(req.body);
-
+    const appointment = new Appointment({
+        docname:req.body.docname,
+        ApptDate:req.body.ApptDate,
+        BookDate:Date.now(),
+        Patient:req.body.Patient,
+        email:req.body.email,
+        phone:req.body.phone,
+        age:req.body.age,
+        gender:req.body.gender,
+    });
     Appointment.create(appointment).then(() => {
         res.json("Appointment added")
         console.log(`--> ${req.method} Response`);
@@ -39,4 +47,44 @@ const getOneAppointment = (req,res)=> {
     })
 }
 
-module.exports={createAppointment,readAllAppointments,getOneAppointment}
+//Update an appointment
+const UpdateAppointment = (req, res) => {
+    console.log(`<-- ${req.method} Request`);
+    const id = req.params.id;
+
+    Appointment.findByIdAndUpdate({
+        _id: id
+    }, {
+        $set: {
+            docname: req.body.docname,
+            ApptDate: req.body.ApptDate,
+            BookDate: req.body.BookDate,
+            Patient: req.body.Patient,
+            email: req.body.email,
+            phone: req.body.phone,
+            age: req.body.age,
+            gender: req.body.gender
+        }
+    }, {
+        new: true
+    }).then(() => {
+        console.log(`--> ${req.method} Response`);
+        res.status(200).json("Appointment updated");
+    }).catch((error) => {
+        console.log(`${error.message}`)
+        res.status(404).json("Appointment not updated");
+    })
+}
+
+//Delete an appointment
+const DeleteAppointment = async (req, res) => {
+    console.log(`<-- ${req.method} Request`);
+    Appointment.findByIdAndDelete({ _id:req.params.id})
+        .then(() => {
+            res.status(200).json("Appointment has been deleted");
+            console.log(`--> ${req.method} Response`);
+        }).catch((error)=>{
+        res.status(500).json(error);
+    })
+};
+module.exports={createAppointment,readAllAppointments,getOneAppointment, DeleteAppointment, UpdateAppointment}
